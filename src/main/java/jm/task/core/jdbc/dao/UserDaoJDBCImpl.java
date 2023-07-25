48ee4ee51178930b7b9a3575f231a8f1184d02db
Maksim Bardikov <maximelanosx@yandex.ru>
2023-07-26 00:39:42 +0300

    задача готова

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,7 +2,11 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+
+import static jm.task.core.jdbc.util.Util.getConnection;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
@@ -10,26 +14,75 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        String sql = "CREATE TABLE IF NOT EXISTS users (ID INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR(100) NOT NULL, LASTNAME VARCHAR(100) NOT NULL, AGE TINYINT NOT NULL)";
+        try (Connection connection = getConnection(); Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        String sql = "DROP TABLE IF EXISTS users";
+        try (Connection connection = getConnection(); Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String sql = "INSERT users (NAME, LASTNAME, AGE) VALUES (?, ?, ?)";
+        try (Connection connection = getConnection(); PreparedStatement preparedstatement = connection.prepareStatement(sql)) {
+            preparedstatement.setString(1, name);
+            preparedstatement.setString(2, lastName);
+            preparedstatement.setByte(3, age);
+            preparedstatement.executeUpdate();
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        String sql = "DELETE FROM users WHERE ID = ?";
+        try (Connection connection = getConnection(); PreparedStatement preparedstatement = connection.prepareStatement(sql)) {
+            preparedstatement.setLong(1, id);
+            preparedstatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+
+        try (Connection connection = getConnection(); Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                User user = new User();
+
+                user.setId(resultSet.getLong("ID"));
+                user.setName(resultSet.getString("NAME"));
+                user.setLastName(resultSet.getString("LASTNAME"));
+                user.setAge(resultSet.getByte("AGE"));
+
+                userList.add(user);
+            }
+            System.out.println(userList.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userList;
     }
 
     public void cleanUsersTable() {
-
+        String sql = "DELETE FROM users";
+        try (Connection connection = getConnection(); Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
